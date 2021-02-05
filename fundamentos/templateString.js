f6cc63f24590f3530be290e65824c20e236620c4
Izaac Baptista <izaacbaptista@gmail.com>
2021-02-04 22:06:54 -0300

    new changes

--- fundamentos/templateString.js
+++ fundamentos/templateString.js
@@ -16,4 +16,4 @@ console.log(`1 + 1 = ${1 + 1}`)
 
 //função para converter para maisculo
 const up = texto => texto.toUpperCase()
-console.log(`Ei... ${up('cuidado)}!`)
+console.log(`Ei... ${up('cuidado')}!`)
