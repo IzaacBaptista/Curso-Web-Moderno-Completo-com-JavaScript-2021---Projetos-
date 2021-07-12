de571378403e7344f67e72daa08dded661af7580
Izaac Baptista <izaacbaptista@gmail.com>
2021-07-12 19:21:55 -0300

    up

---
+++ fundamentos/boolean.js
@@ -0,0 +1,17 @@
+let isAtivo = false
+console.log(isAtivo)
+
+let isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo)
+
+isAtivo = 1
+console.log(!true)
+
+isAtivo = 1
+console.log(!!true)
